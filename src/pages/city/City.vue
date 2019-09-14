413d1a15fe3fbf956d11cd5c31087b62e7031ee1
Lyiran <lyiyi0834@163.com>
2019-09-14 23:37:39 +0800

    search content update

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <city-header>城市</city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
     <city-list :cities="cities" :hot="hotCities" :letter="letter"></city-list>
     <city-alphabet :cities="cities" @change="handleLetterChange"></city-alphabet>
   </div>
--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="search">
-    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content">
+      <ul>
+        <li v-for="item of list" :key="item.id">{{item.name}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -23,4 +58,13 @@ export default {
         text-align: center
         border-radius: .06rem
         color: #666
+    .search-content
+      z-index: 1
+      overflow: hidden
+      position: absolute
+      top: 1.58rem
+      left: 0
+      right: 0
+      bottom: 0
+      background: green
 </style>

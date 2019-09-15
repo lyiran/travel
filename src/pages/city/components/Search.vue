fa8d25dff36ea9fde23797bbba714ea70be7f576
Lyiran <lyiyi0834@163.com>
2019-09-15 23:28:15 +0800

    logic update

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -3,14 +3,16 @@
     <div class="search">
       <input v-model="keyword" class="search-input" type="text" placeholder="输入城市名或拼音">
     </div>
-    <div class="search-content">
+    <div class="search-content" ref="search" v-show="keyword">
       <ul>
-        <li v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-for="item of list" :key="item.id">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">-- 没有找到匹配数据 --</li>
       </ul>
     </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
   name: 'CitySearch',
   props: {
@@ -23,10 +25,19 @@ export default {
       timer: null
     }
   },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
   watch: {
     keyword () {
       if (this.timer) {
         clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
       }
       this.timer = setTimeout(() => {
         const result = []
@@ -40,6 +51,9 @@ export default {
         this.list = result
       }, 100)
     }
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.search)
   }
 }
 </script>
@@ -66,5 +80,10 @@ export default {
       left: 0
       right: 0
       bottom: 0
-      background: green
+      background: #eee
+      .search-item
+        line-height: .62rem
+        padding-left: .2rem
+        background: #fff
+        color: #666
 </style>
